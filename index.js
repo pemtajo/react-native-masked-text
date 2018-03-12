84bfbf1e7d19fd452b74b893d171483229ce4766
EuAndreh <eu@euandre.org>
2018-03-12 07:52:45 -0300

    Restore code

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import MaskService from './lib/mask-service';
 import TextInputMask from './lib/text-input-mask';
 import TextMask from './lib/text-mask';
+import MaskResolver from './lib/mask-resolver';
 
 module.exports.MaskService = MaskService;
 module.exports.TextInputMask = TextInputMask;
 module.exports.TextMask = TextMask;
+module.exports.MaskResolver = MaskResolver;
